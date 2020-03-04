6606df0fd10d9ea18f963d3290471505371dfedc
Álvaro Bacelar <alvarobacelar@outlook.com>
2020-03-04 14:06:41 -0300

    Add files alter for build another launch

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/servlet/security/SslConnectionIntegrationTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/servlet/security/SslConnectionIntegrationTest.java
@@ -6,6 +6,8 @@ package com.linkedin.kafka.cruisecontrol.servlet.security;
 
 import com.linkedin.kafka.cruisecontrol.CruiseControlIntegrationTestHarness;
 import com.linkedin.kafka.cruisecontrol.config.constants.WebServerConfig;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -16,20 +18,34 @@ import javax.net.ssl.X509TrustManager;
 import javax.servlet.http.HttpServletResponse;
 import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import static com.linkedin.kafka.cruisecontrol.servlet.CruiseControlEndPoint.STATE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class SslConnectionIntegrationTest extends CruiseControlIntegrationTestHarness {
 
-  private static final String CRUISE_CONTROL_STATE_ENDPOINT = "kafkacruisecontrol/state";
+  private static final String CRUISE_CONTROL_STATE_ENDPOINT = "kafkacruisecontrol/" + STATE;
+  public static final String HTTPS = "https";
+
+  @Before
+  public void setup() throws Exception {
+    super.start();
+  }
+
+  @After
+  public void teardown() {
+    super.stop();
+  }
 
   @Override
   protected Map<String, Object> withConfigs() {
     Map<String, Object> sslConfigs = new HashMap<>();
+    sslConfigs.put(WebServerConfig.WEBSERVER_SSL_ENABLE_CONFIG, true);
     sslConfigs.put(WebServerConfig.WEBSERVER_SSL_KEYSTORE_LOCATION_CONFIG, Objects.requireNonNull(
         this.getClass().getClassLoader().getResource("ssl_integration_test.keystore")).toString());
     sslConfigs.put(WebServerConfig.WEBSERVER_SSL_KEYSTORE_PASSWORD_CONFIG, "jetty");
@@ -55,6 +71,7 @@ public class SslConnectionIntegrationTest extends CruiseControlIntegrationTestHa
 
   @Test
   public void testSslConnection() throws Exception {
+    assertEquals(HTTPS, new URL(_app.serverUrl()).getProtocol());
     SSLSocketFactory defaultSslSocketFactory = HttpsURLConnection.getDefaultSSLSocketFactory();
     try {
       SSLContext sc = SSLContext.getInstance("SSL");
